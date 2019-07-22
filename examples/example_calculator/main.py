da5749648580bd03991a2985cd0a508ec22d3fca
lovejunjie1 <276415977@qq.com>
2019-07-22 20:17:03 +0800

    first upload
    初次提交

---
+++ examples/example_calculator/main.py
@@ -0,0 +1,16 @@
+import sys
+from PyQt5.QtWidgets import *
+
+from examples.example_calculator.calc_window import CalculatorWindow
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+
+    # print(QStyleFactory.keys())
+    app.setStyle('Fusion')
+
+    wnd = CalculatorWindow()
+    wnd.show()
+
+    sys.exit(app.exec_())
